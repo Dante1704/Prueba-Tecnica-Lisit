fd3f0e12af7a6e8538d291d7920d9e1675741dc4
Dante1704 <dante.nkaddarian@gmail.com>
2023-11-07 21:30:57 -0300

    feat: implementado un layout general para toda la app

--- prueba-tecnica-lisit/src/main.jsx
+++ prueba-tecnica-lisit/src/main.jsx
@@ -1,20 +1,19 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-  }
-])
+import './index.css'
+import { Layout } from './components/Layout'
+import { Home } from './pages/Home'
+import { Personas } from './components/Personas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/personas' element={<Personas />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
