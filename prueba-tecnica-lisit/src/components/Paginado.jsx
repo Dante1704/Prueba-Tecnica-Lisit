01591bb46c565a80714568d3f6ed805684ca7f77
Dante1704 <dante.nkaddarian@gmail.com>
2023-11-09 16:29:17 -0300

    hotfix: eliminados comentarios y console.logs del paginado

--- prueba-tecnica-lisit/src/components/Paginado.jsx
+++ prueba-tecnica-lisit/src/components/Paginado.jsx
@@ -2,19 +2,8 @@ import React from 'react'
 
 export const Paginado = ({ paginaActual, paginaAnterior, paginaSiguiente, cambiarPagina }) => {
 
-
-    /* const [paginado, setPaginado] = useState({
-       min: 1,
-       max: Math.ceil(personasPaginadas.count/10),
-       startPage: 1 //redonde a 9 pag
-   }) */
-
-
-
-
     const irAlAnterior = () => {
         if (paginaAnterior) {
-
             const nuevaPagina = paginaActual - 1
             cambiarPagina(nuevaPagina)
         }
@@ -26,12 +15,7 @@ export const Paginado = ({ paginaActual, paginaAnterior, paginaSiguiente, cambia
             const nuevaPagina = paginaActual + 1
             cambiarPagina(nuevaPagina)
         }
-        console.log(paginaActual);
     }
-
-
-    console.log("paginaAnterior: ", paginaAnterior);
-    console.log("paginaSiguiente: ", paginaSiguiente);
 
     return (
         <div className="join">
